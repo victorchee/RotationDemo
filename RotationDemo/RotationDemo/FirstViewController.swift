221b923875f34c5aaa7ee26f9f4fc67f9a533d41
Victor Chee <victor.qhj@gmail.com>
2016-11-03 09:14:26 +0800

    Add project.

---
+++ RotationDemo/RotationDemo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  RotationDemo
+//
+//  Created by Migu on 2016/11/3.
+//  Copyright © 2016年 VictorChee. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
